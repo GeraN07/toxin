f997b4d5b94fb152759a4981b219152c9298f643
GeraN07 <gera3050@yandex.ru>
2020-06-19 21:00:31 +0300

    provided date-dropdown functionality

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,4 +1,10 @@
-$('#my-element').datepicker([options])
-
-// Доступ к экземпляру объекта
-$('#my-element').data('datepicker')
+$('#start_one').datepicker({ 
+    minDate: new Date(),
+    onSelect: function (fd, d, picker) { 
+      $("#start_one").val(fd.split("-")[0]);
+      $("#end_one").val(fd.split("-")[1]);
+    },
+  });
+  $( '#end_one' ).click(function() {
+    $('#start_one').focus()
+  })
