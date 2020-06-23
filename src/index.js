9e20a7d834a7d221b10b2d0b83f884583a6e76ee
GeraN07 <gera3050@yandex.ru>
2020-06-23 23:02:06 +0300

    rating plugin added, ui-kit page has been supplemented

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ require("../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.j
 require("../node_modules/air-datepicker/dist/js/datepicker.min.js")
 require("../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js")
 require("../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js")
-require("../node_modules/jquery-like-button-rating-master/jquery.likebtn.js")
+require("../src/assets/Minimal-Rating-Plugin-With-jQuery-And-Material-Icons-star-rating-js/src/jquery.star.rating.min.js")
+
 //Pages
 require("./pages/ui-kit.pug");
 require("./pages/ui-kit.js");
--- src/pages/ui-kit.js
+++ src/pages/ui-kit.js
@@ -2,4 +2,5 @@ require("../components/dropdown/dropdown.js");
 require("../components/double-date-dropdown/double-date-dropdown.js");
 require("../components/filter-date-dropdown/filter-date-dropdown.js");
 require("../components/range-slider/range-slider.js");
+require("../components/like-button/like-button.js");
 require("../components/rate-button/rate-button.js");
