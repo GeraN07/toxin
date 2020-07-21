9084b0df9b7400781026bd380b2ef99efc7d967b
GeraN07 <gera3050@yandex.ru>
2020-07-21 22:32:06 +0300

    Form Elements page complited, card page in progress

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var jquery = require("../node_modules/jquery/dist/jquery.js");
 window.$ = window.jQuery = jquery;
 import * as $ from 'jquery'
 require("../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js")
-require("../node_modules/air-datepicker/dist/js/datepicker.min.js")
+
 require("../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js")
 require("../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js")
 require("../src/assets/Minimal-Rating-Plugin-With-jQuery-And-Material-Icons-star-rating-js/src/jquery.star.rating.min.js")
-
+require("../src/cards/datepicker-card/datepicker-card.js")
 //Pages
 require("./pages/ui-kit.pug");
 require("./pages/ui-kit.js");
--- src/pages/ui-kit.js
+++ src/pages/ui-kit.js
@@ -4,3 +4,5 @@ require("../components/filter-date-dropdown/filter-date-dropdown.js");
 require("../components/range-slider/range-slider.js");
 require("../components/like-button/like-button.js");
 require("../components/rate-button/rate-button.js");
+require("../components/expandable-checkbox-list/expandable-checkbox-list.js");
+require("../cards/datepicker-card/datepicker-card.js");
