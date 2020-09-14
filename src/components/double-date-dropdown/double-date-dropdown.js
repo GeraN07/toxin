e32e8b3cd9b9362afe08a256b15b7198fef68503
GeraN07 <gera3050@yandex.ru>
2020-09-14 20:31:09 +0300

    added open/close animation for double-date-dropdown

--- src/components/double-date-dropdown/double-date-dropdown.js
+++ src/components/double-date-dropdown/double-date-dropdown.js
@@ -10,3 +10,29 @@ $(".first-date").datepicker({
 $(".dates").click(function() {
   $(".first-date").focus()
 })
+// $(".first-date").focus(function(){
+//   $(this).parent().toggleClass("datepicker-activated")
+// })
+$(".first-date").click(function() {
+  $(this).parent().toggleClass("datepicker-activated")
+});
+$(".second-date").click(function() {
+  $(this).parent().toggleClass("datepicker-activated")
+});
+
+$(document).mouseup(function (e){
+  var block = $(".date-1");
+  var block1 = $(".datepickers-container");
+  if (!block.is(e.target) 
+      && block.has(e.target).length === 0 && !block1.is(e.target) && block1.has(e.target).length === 0 ) {
+      block.removeClass('datepicker-activated');;
+  }
+});
+$(document).mouseup(function (e){
+  var block = $(".date-2");
+  var block1 = $(".datepickers-container");
+  if (!block.is(e.target) 
+      && block.has(e.target).length === 0 && !block1.is(e.target) && block1.has(e.target).length === 0 ) {
+      block.removeClass('datepicker-activated');;
+  }
+});
