0db45ed682358272a7308cfb0a1085e697c4f5a7
GeraN07 <gera3050@yandex.ru>
2020-11-09 14:01:32 +0300

    room-details anchor-button and responsive added

--- src/pages/room-details.js
+++ src/pages/room-details.js
@@ -1,19 +1,32 @@
+require("../assets/Revealator-plugin/fm.revealator.jquery.js");
 
-var speed = 4;
-function incEltNbr(id) {
-  elt = document.getElementById(id);
-  endNbr = Number(document.getElementById(id).innerHTML);
-  incNbrRec(0, endNbr, elt);
-}
+// var speed = 4;
+// function incEltNbr(id) {
+//   elt = document.getElementById(id);
+//   endNbr = Number(document.getElementById(id).innerHTML);
+//   incNbrRec(0, endNbr, elt);
+// }
 
-function incNbrRec(i, endNbr, elt) {
-  if (i <= endNbr) {
-    elt.innerHTML = i;
-    setTimeout(function() {
-      incNbrRec(i + 1, endNbr, elt);
-    }, speed);
+// function incNbrRec(i, endNbr, elt) {
+//   if (i <= endNbr) {
+//     elt.innerHTML = i;
+//     setTimeout(function() {
+//       incNbrRec(i + 1, endNbr, elt);
+//     }, speed);
+//   }
+// }
+
+
+// $(document).ready(incEltNbr("nbr"));
+
+jQuery(function(f){
+    var element = f('#anchor-button');
+    if (window.innerWidth > 1000) { 
+      $('#anchor-button').hide() 
   }
-}
-
-
-incEltNbr("nbr");
+    if (window.innerWidth < 1000)  {
+      f(window).scroll(function(){
+          ((window.innerWidth < 1000)? element['fade'+ ((f(this).scrollTop() > 1150) ? 'Out': 'In')](500): $('#anchor-button').hide())         
+      });
+  }
+});
