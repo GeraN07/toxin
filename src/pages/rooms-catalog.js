782c1a13e57336d80b221f33d7705c03c1244740
GeraN07 <gera3050@yandex.ru>
2020-11-09 14:03:08 +0300

    rooms-catalog responsive added

---
+++ src/pages/rooms-catalog.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.button-purpule-large').click(function() {
+        $('.rooms-catalog__filter-block').toggleClass('active')
+    })
+    $('.filter-block__close-button').click(function() {
+        $('.rooms-catalog__filter-block').toggleClass('active')
+    })
+})
