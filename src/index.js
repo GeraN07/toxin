034e5d1178f358f4e8bff5d7338feec0d00d6ddc
GeraN07 <gera3050@yandex.ru>
2020-12-26 18:40:15 +0300

    preloader added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-//Jquery
-var jquery = require("../node_modules/jquery/dist/jquery.js");
-window.$ = window.jQuery = jquery;
-import * as $ from 'jquery'
 // require("../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js")
 // require("../node_modules/item-quantity-dropdown/src/index.js")
 require("../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js")
@@ -10,8 +6,9 @@ require("../src/assets/Minimal-Rating-Plugin-With-jQuery-And-Material-Icons-star
 require("../src/cards/datepicker-card/datepicker-card.js")
 require("../src/components/dropdown/dropdown.js")
 require("../src/components/header-main/header-main.js")
+require("../src/components/preloader/preloader.js")
 //Pages
-require("./pages/ui-kit.pug");
+
 require("./pages/ui-kit.js");
 require("./pages/room-details.js");
 require("./pages/rooms-catalog.js");
