aea660d590c3491d7715a024fa44df5943b88e34
GeraN07 <gera3050@yandex.ru>
2020-12-26 13:25:21 +0300

    BEM and css corrected

--- src/components/header-main/header-main.js
+++ src/components/header-main/header-main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('.burger-button').click(function() {
         $('.burger-button__icon').toggleClass('active');
-        $('.page-header--nav').toggleClass('active');
+        $('.page-header__nav').toggleClass('active');
         $('.page-header__button-block').toggleClass('active');
         $('.filter-main').toggleClass('moved-down');
     })
-    $('.page-header--services').click(function() {
-        $('.page-header-sevices-list').toggleClass('active');
+    $('.page-header__services').click(function() {
+        $('.page-header__sevices-list').toggleClass('active');
     })
-    $('.page-header--agree').click(function() {
-        $('.page-header-agree-list').toggleClass('active');
+    $('.page-header__agree').click(function() {
+        $('.page-header__agree-list').toggleClass('active');
     })
 })
