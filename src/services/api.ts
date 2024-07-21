2f3a39e7f17856d537c493fde38edbae6ec7b00b
GeraN7 <gera3050@yandex.ru>
2024-07-21 16:59:15 +0300

    api path upgade

--- src/services/api.ts
+++ src/services/api.ts
@@ -16,7 +16,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 const shouldDisplayError = (response: AxiosResponse) =>
   !!StatusCodeMapping[response.status];
 
-const BACKEND_URL = 'https://toxin-eight.vercel.app/';
+const BACKEND_URL = '/';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
