17d1d7507415c5c394557fb940cd30df235fd5fc
GeraN7 <gera3050@yandex.ru>
2024-07-24 23:16:43 +0300

    filter state in URL created

--- src/cards/aside-filters/aside-filters.tsx
+++ src/cards/aside-filters/aside-filters.tsx
@@ -1,33 +1,79 @@
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CheckboxButtons from '../../components/checkbox-buttons/checkbox-buttons';
 import Dropdown from '../../components/dropdown-qty/dropdown-qty';
 import ExpandableCheckboxList from '../../components/expandable-checkbox-list/expandable-checkbox-list';
 import FilterDateDropdown from '../../components/filter-date-dropdown/filter-date-dropdown';
 import RangeSlider from '../../components/range-slider/range-slider';
 import RichCheckboxButtons from '../../components/rich-checkbox-buttons/rich-checkbox-buttons';
+import { Store } from '../../types/state';
 
 type AsideFiltersProps = {
   asideOpen: boolean;
   toggleAside: () => void;
 };
 
-const AsideFilters = ({ asideOpen, toggleAside }: AsideFiltersProps) => (
-  <aside className={`filter-block ${asideOpen ? 'active' : ''}`}>
-    <span className="filter-block__close-button" onClick={toggleAside}>
-      <div className="filter-block__close-button-icon"></div>
-    </span>
-    <div className="filter-block__filter-block-wrapper">
-      <FilterDateDropdown />
-      <Dropdown option={'guest'}/>
-      <RangeSlider h3="ДИАПАЗОН ЦЕНЫ" />
-      <CheckboxButtons h3="CHECKBOX BUTTONS" />
-      <RichCheckboxButtons h3="ДОСТУПНОСТЬ" />
-      <Dropdown option={'rooms'}/>
-      <ExpandableCheckboxList buttonTitle="дополнительные удобства" />
-      <span className="button-purpule-large">
-        <a className="button-purpule-large__link" onClick={toggleAside}>применить</a>
+const AsideFilters = ({ asideOpen, toggleAside }: AsideFiltersProps) => {
+  const filters = useSelector((state: Store) => state);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const updateURL = (filters: Store) => {
+      const params = new URLSearchParams();
+
+      if (filters.maxGuests !== undefined && filters.maxGuests !== 1) params.set('maxGuests', filters.maxGuests.toString());
+      if (filters.adultCount !== undefined && filters.adultCount !== 0) params.set('adultCount', filters.adultCount.toString());
+      if (filters.childCount !== undefined && filters.childCount !== 0) params.set('childCount', filters.childCount.toString());
+      if (filters.infantCount !== undefined && filters.infantCount !== 0) params.set('infantCount', filters.infantCount.toString());
+      if (filters.datesRange[0] && filters.datesRange.length > 0) {
+        params.set('datesRange', filters.datesRange.join(','));
+      }
+      if (filters.minPrice !== undefined && filters.minPrice !== 0) params.set('minPrice', filters.minPrice.toString());
+      if (filters.maxPrice !== undefined && filters.maxPrice !== 16000) params.set('maxPrice', filters.maxPrice.toString());
+      if (filters.smoking !== undefined) params.set('smoking', filters.smoking.toString());
+      if (filters.pet !== undefined) params.set('pet', filters.pet.toString());
+      if (filters.guests !== undefined) params.set('guests', filters.guests.toString());
+      if (filters.wideCoridor !== undefined) params.set('wideCoridor', filters.wideCoridor.toString());
+      if (filters.helper !== undefined) params.set('helper', filters.helper.toString());
+      if (filters.bedroomCount !== undefined && filters.bedroomCount !== 0) params.set('bedroomCount', filters.bedroomCount.toString());
+      if (filters.bedsCount !== undefined && filters.bedsCount !== 0) params.set('bedsCount', filters.bedsCount.toString());
+      if (filters.bathRoomsCount !== undefined && filters.bathRoomsCount !== 0) params.set('bathrooms', filters.bathRoomsCount.toString());
+      if (filters.breakfast !== undefined) params.set('breakfast', filters.breakfast.toString());
+      if (filters.table !== undefined) params.set('table', filters.table.toString());
+      if (filters.hchair !== undefined) params.set('hchair', filters.hchair.toString());
+      if (filters.babyBed !== undefined) params.set('babyBed', filters.babyBed.toString());
+      if (filters.tv !== undefined) params.set('tv', filters.tv.toString());
+      if (filters.shampoo !== undefined) params.set('shampoo', filters.shampoo.toString());
+
+      // Обновление URL с использованием navigate
+      navigate(`${window.location.pathname}?${params.toString()}`, { replace: true });
+    };
+
+    updateURL(filters);
+  }, [filters, navigate]);
+
+  return (
+    <aside className={`filter-block ${asideOpen ? 'active' : ''}`}>
+      <span className="filter-block__close-button" onClick={toggleAside}>
+        <div className="filter-block__close-button-icon"></div>
       </span>
-    </div>
-  </aside>
-);
+      <div className="filter-block__filter-block-wrapper">
+        <FilterDateDropdown />
+        <Dropdown option={'guest'} />
+        <RangeSlider h3="ДИАПАЗОН ЦЕНЫ" />
+        <CheckboxButtons h3="CHECKBOX BUTTONS" />
+        <RichCheckboxButtons h3="ДОСТУПНОСТЬ" />
+        <Dropdown option={'rooms'} />
+        <ExpandableCheckboxList buttonTitle="дополнительные удобства" />
+        <span className="button-purpule-large">
+          <a className="button-purpule-large__link" onClick={toggleAside}>
+            применить
+          </a>
+        </span>
+      </div>
+    </aside>
+  );
+};
 
 export default AsideFilters;
--- src/pages/rooms/rooms-catalog.tsx
+++ src/pages/rooms/rooms-catalog.tsx
@@ -9,7 +9,31 @@ import HeaderMain from '../../components/header-main/header-main';
 import RoomsCatalogList from '../../components/catalog-block/catalog-block';
 import Pagination from '../../components/pagination/pagination';
 import SmallFooter from '../../components/small-footer/small-footer';
-import { setSortedRooms } from '../../store/action';
+import { useSearchParams } from 'react-router-dom';
+import {
+  setSortedRooms,
+  setMaxGuests,
+  setAdultCount,
+  setChildCount,
+  setInfantCount,
+  setDatesRange,
+  setMinPrice,
+  setMaxPrice,
+  setSmoking,
+  setPet,
+  setGuests,
+  setWideCoridor,
+  setHelper,
+  setBedrooms,
+  setBeds,
+  setBathrooms,
+  setBreakfast,
+  setTable,
+  setHchair,
+  setBabyBed,
+  setTv,
+  setShampoo,
+} from '../../store/action';
 import './rooms-catalog.css';
 import { getAllRooms, getSortedRooms } from '../../store/selectors';
 import Preloader from '../../components/preloader/preloader';
@@ -22,6 +46,75 @@ const RoomCatalog = () => {
   const [roomsPerPage] = useState(12);
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(true);
+
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.has('maxGuests')) {
+      dispatch(setMaxGuests(Number(searchParams.get('maxGuests'))));
+    }
+    if (searchParams.has('adultCount')) {
+      dispatch(setAdultCount(Number(searchParams.get('adultCount'))));
+    }
+    if (searchParams.has('childCount')) {
+      dispatch(setChildCount(Number(searchParams.get('childCount'))));
+    }
+    if (searchParams.has('infantCount')) {
+      dispatch(setInfantCount(Number(searchParams.get('infantCount'))));
+    }
+    if (searchParams.has('datesRange')) {
+      const datesRange = searchParams.get('datesRange');
+      if (datesRange) dispatch(setDatesRange(datesRange.split(',')));
+    }
+    if (searchParams.has('minPrice')) {
+      dispatch(setMinPrice(Number(searchParams.get('minPrice'))));
+    }
+    if (searchParams.has('maxPrice')) {
+      dispatch(setMaxPrice(Number(searchParams.get('maxPrice'))));
+    }
+    if (searchParams.has('smoking')) {
+      dispatch(setSmoking(searchParams.get('smoking') === 'true'));
+    }
+    if (searchParams.has('pet')) {
+      dispatch(setPet(searchParams.get('pet') === 'true'));
+    }
+    if (searchParams.has('guests')) {
+      dispatch(setGuests(searchParams.get('guests') === 'true'));
+    }
+    if (searchParams.has('wideCoridor')) {
+      dispatch(setWideCoridor(searchParams.get('wideCoridor') === 'true'));
+    }
+    if (searchParams.has('helper')) {
+      dispatch(setHelper(searchParams.get('helper') === 'true'));
+    }
+    if (searchParams.has('bedroomCount')) {
+      dispatch(setBedrooms(Number(searchParams.get('bedroomCount'))));
+    }
+    if (searchParams.has('bedsCount')) {
+      dispatch(setBeds(Number(searchParams.get('bedsCount'))));
+    }
+    if (searchParams.has('bathrooms')) {
+      dispatch(setBathrooms(Number(searchParams.get('bathrooms'))));
+    }
+    if (searchParams.has('breakfast')) {
+      dispatch(setBreakfast(searchParams.get('breakfast') === 'true'));
+    }
+    if (searchParams.has('table')) {
+      dispatch(setTable(searchParams.get('table') === 'true'));
+    }
+    if (searchParams.has('hchair')) {
+      dispatch(setHchair(searchParams.get('hchair') === 'true'));
+    }
+    if (searchParams.has('babyBed')) {
+      dispatch(setBabyBed(searchParams.get('babyBed') === 'true'));
+    }
+    if (searchParams.has('tv')) {
+      dispatch(setTv(searchParams.get('tv') === 'true'));
+    }
+    if (searchParams.has('shampoo')) {
+      dispatch(setShampoo(searchParams.get('shampoo') === 'true'));
+    }
+  }, [searchParams, dispatch]);
 
   useEffect(() => {
     dispatch(setSortedRooms());
@@ -50,7 +143,10 @@ const RoomCatalog = () => {
 
   const lastRoomIndex = currentPage * roomsPerPage;
   const firstRoomIndex = lastRoomIndex - roomsPerPage;
-  const currentRooms = useMemo(() => rooms.slice(firstRoomIndex, lastRoomIndex), [rooms, firstRoomIndex, lastRoomIndex]);
+  const currentRooms = useMemo(
+    () => rooms.slice(firstRoomIndex, lastRoomIndex),
+    [rooms, firstRoomIndex, lastRoomIndex]
+  );
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   const prevPage = () => setCurrentPage((prev) => prev - 1);
