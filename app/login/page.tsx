27e9793aaf8674d25dbc5986a2e1285676bc2dcb
GeraN7 <gera3050@yandex.ru>
2025-06-06 01:23:24 +0300

    Login page refactoring

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,43 +1,10 @@
-'use client';
-import { useEffect, useMemo, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import LoginCard from '../components/cards/login-card/login-card';
-import RegistrationCard from '../components/cards/registration-card/registration-card';
-import BigFooter from '../components/big-footer/big-footer';
-import HeaderMain from '../components/header-main/header-main';
-import SmallFooter from '../components/small-footer/small-footer';
-import './login-page.css';
+import { Suspense } from 'react';
+import LoginPage from './login';
 
-const LoginPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
-  const searchParams = useSearchParams();
-
-  const param = useMemo(() => searchParams.get('param'), [searchParams]);
-
-  useEffect(() => {
-    setIsLogin(param === 'Log');
-  }, [param]);
-
-  useEffect(() => {
-    document.title = isLogin ? 'Вход в аккаунт' : 'Регистрация';
-  }, [isLogin]);
-
-  const toggleForm = () => setIsLogin((prev) => !prev);
-
+export default function Page() {
   return (
-    <div className="login-page">
-      <HeaderMain />
-      <section className="login-block">
-        {isLogin ? (
-          <LoginCard onToggleForm={toggleForm} />
-        ) : (
-          <RegistrationCard onToggleForm={toggleForm} />
-        )}
-      </section>
-      <BigFooter />
-      <SmallFooter />
-    </div>
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <LoginPage />
+    </Suspense>
   );
-};
-
-export default LoginPage;
+}
