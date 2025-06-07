536dffdf0df078f0c7f8dc82c921e2ad33728831
GeraN7 <gera3050@yandex.ru>
2025-06-07 16:43:06 +0300

    rooms-catalog refactoring

--- app/rooms-catalog/page.tsx
+++ app/rooms-catalog/page.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
 import BigFooter from '../components/big-footer/big-footer';
+import { Suspense } from 'react';
 import HeaderMain from '../components/header-main/header-main';
 import SmallFooter from '../components/small-footer/small-footer';
 import ClientRoomsCatalogList from '../components/client-rooms-catalog-list/client-rooms-catalog-list';
 import './rooms-catalog.css';
+import ComponentPreloader from '../components/component-preloader/component-preloader';
 
 export const metadata = {
   title: 'Каталог номеров',
@@ -15,7 +17,9 @@ const RoomCatalog = () => (
     <HeaderMain />
     <main className="rooms-catalog__main">
       <div className="rooms-catalog__main-wrapper">
-        <ClientRoomsCatalogList />
+        <Suspense fallback={<ComponentPreloader />}>
+          <ClientRoomsCatalogList />
+        </Suspense>
       </div>
     </main>
     <BigFooter />
