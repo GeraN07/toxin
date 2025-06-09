b0cb2a2469a0ce43ee94dd90ae3f9dda968635b1
GeraN7 <gera3050@yandex.ru>
2025-06-09 23:34:55 +0300

    route refactoring

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -2,17 +2,11 @@ import { NextResponse } from 'next/server';
 import { getCachedRooms } from '../../../utils/roomCache';
 import { FullOffer } from '../../../types/rooms';
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export async function GET(_: Request, { params }: Params) {
+export async function GET(_: Request, context: { params: { id: string } }) {
+  const { id } = context.params;
   const { fullRooms } = getCachedRooms();
-  const fullRoom = fullRooms.find(
-    (fullRoom: FullOffer) => fullRoom.id === params.id
-  );
+
+  const fullRoom = fullRooms.find((room: FullOffer) => room.id === id);
 
   if (!fullRoom) {
     return NextResponse.json({ message: 'Not found' }, { status: 404 });
