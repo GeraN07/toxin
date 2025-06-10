5125c416ef104d425cf638c5e3960fff6c0a74d0
GeraN07 <56051662+GeraN07@users.noreply.github.com>
2025-06-10 21:15:32 +0300

    cors added

--- api/rooms.js
+++ api/rooms.js
@@ -209,6 +209,16 @@ let rooms = createRooms();
 fullRooms = createFullRooms(rooms);
 
 export default function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { id } = req.query;
   console.log(`Received request for room id: ${id}`);
   if (id) {
